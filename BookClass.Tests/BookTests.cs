330143c3e1fdd0548f03a3100fe90e8dd893a6ac
irakli imesashvili <i.imesashvili@betsolutions.com>
2021-12-29 15:13:42 +0400

    Finished

---
+++ BookClass.Tests/BookTests.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using NUnit.Framework;
+
+#pragma warning disable SA1600 // ElementsMustBeDocumented
+
+namespace BookClass.Tests
+{
+    [TestFixture]
+    public sealed class BookTests
+    {
+        [TestCase(5, ExpectedResult = 5)]
+        [TestCase(350, ExpectedResult = 350)]
+        [TestCase(500, ExpectedResult = 500)]
+        [TestCase(2500, ExpectedResult = 2500)]
+        [TestCase(3788, ExpectedResult = 3788)]
+        public int PropertyPagesTest(int value)
+        {
+            Book test = new Book()
+            {
+                Pages = value,
+            };
+
+            return test.Pages;
+        }
+
+        [TestCase(-2)]
+        [TestCase(-8)]
+        [TestCase(0)]
+        [TestCase(-200)]
+        public void PropertyPagesExceptionHandlingTest(int value)
+        {
+            // throws Exception when pages value is less then or equel to 0
+            Assert.Throws<ArgumentException>(() => { new Book { Pages = value }; });
+        }
+
+        [TestCase("FRANK HERBERT", "Dune", "Prospero")]
+        [TestCase("MARCUS ZUSAK", "THE BOOK THIEF", "InterntaionalBooks")]
+        [TestCase("J. K. ROWLING", "HARRY POTTER", "PalitraL")]
+        public void FirstConstructorTest(string author, string title, string publisher)
+        {
+            Book test = new Book(author, title, publisher);
+            Assert.AreEqual(author, test.Author);
+            Assert.AreEqual(title, test.Title);
+            Assert.AreEqual(publisher, test.Publisher);
+        }
+
+        [TestCase("FRANK HERBERT", "Dune", "Prospero", "12345678910")]
+        [TestCase("MARCUS ZUSAK", "THE BOOK THIEF", "InterntaionalBooks", "12345678910")]
+        [TestCase("J. K. ROWLING", "HARRY POTTER", "PalitraL", "12345678910")]
+        public void SecondConstructorTest(string author, string title, string publisher, string isbn)
+        {
+            Book test = new Book(author, title, publisher, isbn);
+            Assert.AreEqual(author, test.Author);
+            Assert.AreEqual(title, test.Title);
+            Assert.AreEqual(publisher, test.Publisher);
+            Assert.AreEqual(isbn, test.ISBN);
+        }
+
+        [TestCase(2009, 10, 11)]
+        [TestCase(2001, 5, 9)]
+        [TestCase(1998, 12, 1)]
+        [TestCase(1990, 9, 5)]
+        public void PublishMethodTest(int year, int month, int day)
+        {
+            DateTime date = new DateTime(year, month, day);
+            Book test = new Book();
+            test.Publish(date);
+            Assert.AreEqual(date.ToString(CultureInfo.InvariantCulture), test.GetPublicationDate());
+        }
+
+        [TestCase(false, ExpectedResult = "NYP")]
+        [TestCase(true, ExpectedResult = "works correctly")]
+        public string GetPublicationDataMethodTest(bool published)
+        {
+            Book test = new Book();
+
+            if (published)
+            {
+                DateTime date = DateTime.Now;
+                test.Publish(date);
+                Assert.AreEqual(date.ToString(CultureInfo.InvariantCulture), test.GetPublicationDate());
+                return "works correctly";
+            }
+            else
+            {
+                Assert.AreEqual("NYP", test.GetPublicationDate());
+                return test.GetPublicationDate();
+            }
+        }
+
+        [TestCase(0.5, "USD")]
+        [TestCase(1, "GEL")]
+        [TestCase(523.5, "EUR")]
+        [TestCase(523.5, "EUR")]
+        [TestCase(32.5, "AED")]
+        [TestCase(10000, "AMD")]
+        public void SetPriceMethodTest(double price, string currency)
+        {
+            Book test = new Book();
+            test.SetPrice(price, currency);
+            Assert.AreEqual(price, test.Price);
+            Assert.AreEqual(currency, test.Currency);
+        }
+
+        [TestCase(-1, "USD")] // Price is invalid
+        [TestCase(0, "GEL")] // Price is invalid
+        [TestCase(3.5, "USA")] // currency doesn't exist
+        [TestCase(315.20, "GALL")] // currency doesn't exist
+        public void SetPriceMethodExceptionHandling(double price, string currency) // throws if price is negative, Currency is lower case  or contains more that 3 character or non letter characters  
+        {
+            Book test = new Book();
+            Assert.Throws<ArgumentException>(() => { test.SetPrice(price, currency); });
+        }
+
+        [TestCase("Dune", "FRANK HERBERT", ExpectedResult = "Dune by FRANK HERBERT")]
+        [TestCase("THE BOOK THIEF", "MARCUS ZUSAK", ExpectedResult = "THE BOOK THIEF by MARCUS ZUSAK")]
+        [TestCase("HARRY POTTER", "J. K. ROWLING", ExpectedResult = "HARRY POTTER by J. K. ROWLING")]
+        public string ToStringMethodTest(string title, string author)
+        {
+            Book test = new Book(author, title, " ");
+            return test.ToString();
+        }
+    }
+}
---
+++ BookClass/Book.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace BookClass
+{
+    public sealed class Book
+    {
+        private bool published;
+        private int totalPages;
+        private DateTime datePublished;
+
+
+        public string Title { get; }
+
+        public int Pages
+        {
+            get { return totalPages; }
+            set { totalPages = value > 0 ? value : throw new ArgumentException("Number of pages was invalid"); }
+        }
+
+        public string Publisher { get; }
+        public string ISBN { get; }
+        public string Author { get; }
+        public double Price { get; private set; }
+        public string Currency { get; private set; }
+
+        public Book()
+        { }
+        public Book(string author, string title, string publisher)
+        {
+            this.Author = author ?? throw new ArgumentNullException(nameof(author));
+            this.Title = title ?? throw new ArgumentNullException(nameof(title));
+            this.Publisher = publisher ?? throw new ArgumentNullException(nameof(publisher));
+        }
+        public Book(string author, string title, string publisher, string isbn)
+            : this(author, title, publisher)
+        {
+            if (string.IsNullOrEmpty(isbn))
+            {
+                throw new ArgumentException($"{isbn} => ISBN must have a value");
+            }
+
+            if (isbn.Length < 10 || isbn.Length > 13)
+            {
+                throw new ArgumentException("ISBN should contain 10 to 13 digits");
+            }
+
+            foreach (char c in isbn)
+            {
+                if (!char.IsDigit(c))
+                {
+                    throw new ArgumentException($"{isbn} => ISBN number should only contain numbers");
+                }
+            }
+
+            this.ISBN = isbn;
+        }
+        public void Publish(DateTime date)
+        {
+            this.datePublished = date;
+            this.published = true;
+        }
+
+        public string GetPublicationDate()
+        {
+            if (published)
+            {
+                return datePublished.ToString(CultureInfo.InvariantCulture);
+            }
+            else
+            {
+                return "NYP";
+            }
+        }
+
+        public new string ToString()
+        {
+            return $"{Title} by {Author}";
+        }
+
+        public void SetPrice(double price, string currency)
+        {
+            CultureInfo[] cultures = CultureInfo.GetCultures(CultureTypes.AllCultures & ~CultureTypes.NeutralCultures);
+
+            List<string> currencies = new List<string>();
+
+            foreach (CultureInfo culture in cultures)
+            {
+                if (culture.LCID == 127 || culture.IsNeutralCulture) continue;
+                RegionInfo region = new RegionInfo(culture.LCID);
+                currencies.Add(region.ISOCurrencySymbol);
+            }
+
+            if (currencies.Contains(currency))
+            {
+                this.Currency = currency;
+            }
+            else
+            {
+                throw new ArgumentException($"{currency} => this currency does not exist");
+            }
+
+            if (price > 0)
+            {
+                this.Price = price;
+            }
+            else
+            {
+                throw new ArgumentException($"{price} => This price is invalid");
+            }
+        }
+    }
+}
